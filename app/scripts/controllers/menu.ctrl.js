5f57e48b78bf4e56d7a75da826d8ee7704e6ca3d
Tommi Kenakkkala <tommi.kenakkala@tieto.com>
2016-03-01 16:25:43 +0200

    Menu code formatting

--- app/scripts/controllers/menu.ctrl.js
+++ app/scripts/controllers/menu.ctrl.js
@@ -12,82 +12,82 @@
 * Controller of the dashboard
 */
 angular.module('dashboard')
-.controller('menuCtrl', ['$log', '$state', '$rootScope', 'DEVICE', 'AhjoMeetingsSrv', 'HOMEMODE', 'APPSTATE', 'SigningOpenApi', function ($log, $state, $rootScope, DEVICE, AhjoMeetingsSrv, HOMEMODE, APPSTATE, SigningOpenApi) {
-    $log.log("menuCtrl: CONTROLLER");
-    var self = this;
-    self.title = 'Ahjo Dashboard';
-    self.mtgCount = null;
-    self.sgnCount = null;
-    self.loadingMtg = false;
-    self.loadingSgn = false;
+    .controller('menuCtrl', ['$log', '$state', '$rootScope', 'DEVICE', 'AhjoMeetingsSrv', 'HOMEMODE', 'APPSTATE', 'SigningOpenApi', function ($log, $state, $rootScope, DEVICE, AhjoMeetingsSrv, HOMEMODE, APPSTATE, SigningOpenApi) {
+        $log.log("menuCtrl: CONTROLLER");
+        var self = this;
+        self.title = 'Ahjo Dashboard';
+        self.mtgCount = null;
+        self.sgnCount = null;
+        self.loadingMtg = false;
+        self.loadingSgn = false;
 
-    function getMeetings() {
-        AhjoMeetingsSrv.getMeetings()
-            .then(function (response) {
-                $log.debug("menuCtrl.getMeetings: getMeetings then:");
-                self.mtgCount = 0;
-                if (response && response.objects instanceof Array) {
-            var dt = new Date();
-            dt.setFullYear(dt.getFullYear() - 4);  // this if for testing. to be removed
-            var date = dt.toJSON();
+        function getMeetings() {
+            AhjoMeetingsSrv.getMeetings()
+                .then(function (response) {
+                    $log.debug("menuCtrl.getMeetings: getMeetings then:");
                     self.mtgCount = 0;
+                    if (response && response.objects instanceof Array) {
+                        var dt = new Date();
+                        dt.setFullYear(dt.getFullYear() - 4);  // this if for testing. to be removed
+                        var date = dt.toJSON();
+                        self.mtgCount = 0;
 
-                    for (var i = 0; i < response.objects.length; i++) {
-                        var item = response.objects[i];
-                        if (date && item && (date < item.meetingTime)) {
-                            self.mtgCount++;
+                        for (var i = 0; i < response.objects.length; i++) {
+                            var item = response.objects[i];
+                            if (date && item && (date < item.meetingTime)) {
+                                self.mtgCount++;
+                            }
                         }
                     }
-                }
+                },
+                    function (error) {
+                        $log.error("menuCtrl.getMeetings: getMeetings error: " + JSON.stringify(error));
+                    },
+                    function (notify) {
+                        $log.debug("menuCtrl.getMeetings: getMeetings notify: " + JSON.stringify(notify));
+                        self.loadingMtg = true;
+                    })
+                .finally(function () {
+                    $log.debug("menuCtrl.getMeetings: getMeetings finally");
+                    self.loadingMtg = false;
+                });
+        }
+
+        function getOpenSignings() {
+            //$log.debug("menuCtrl: SigningOpenApi.query open");
+            self.loadingSgn = true;
+            self.responseOpen = SigningOpenApi.query(function () {
+                $log.debug("menuCtrl.getOpenSignings: SigningOpenApi.query open done: " + self.responseOpen.length);
+                self.sgnCount = self.responseOpen.length;
+                self.errOpen = null;
             },
                 function (error) {
-                    $log.error("menuCtrl.getMeetings: getMeetings error: " + JSON.stringify(error));
-                },
-                function (notify) {
-                    $log.debug("menuCtrl.getMeetings: getMeetings notify: " + JSON.stringify(notify));
-                    self.loadingMtg = true;
-                })
-            .finally(function () {
-                $log.debug("menuCtrl.getMeetings: getMeetings finally");
-                self.loadingMtg = false;
+                    $log.error("menuCtrl.getOpenSignings: SigningOpenApi.query open error: " + JSON.stringify(error));
+                    self.errOpen = error;
+                });
+            self.responseOpen.$promise.finally(function () {
+                $log.debug("menuCtrl.getOpenSignings: SigningOpenApi.query open finally");
+                self.responseOpen = null;
+                self.loadingSgn = false;
             });
-    }
+        }
 
-    function getOpenSignings() {
-        //$log.debug("menuCtrl: SigningOpenApi.query open");
-        self.loadingSgn = true;
-        self.responseOpen = SigningOpenApi.query(function () {
-            $log.debug("menuCtrl.getOpenSignings: SigningOpenApi.query open done: " + self.responseOpen.length);
-            self.sgnCount = self.responseOpen.length;
-            self.errOpen = null;
-        },
-            function (error) {
-                $log.error("menuCtrl.getOpenSignings: SigningOpenApi.query open error: " + JSON.stringify(error));
-                self.errOpen = error;
-            });
-        self.responseOpen.$promise.finally(function () {
-            $log.debug("menuCtrl.getOpenSignings: SigningOpenApi.query open finally");
-            self.responseOpen = null;
-            self.loadingSgn = false;
-        });
-    }
+        // PUBLIC FUNCTIONS
+        self.showMeetings = function () {
+            $log.debug("menuCtrl: showMeetings" + HOMEMODE.MEETINGS);
+            $state.go(APPSTATE.OVERVIEW, { state: HOMEMODE.MEETINGS });
+        };
 
-    // PUBLIC FUNCTIONS
-    self.showMeetings = function() {
-        $log.debug("menuCtrl: showMeetings" +HOMEMODE.MEETINGS);
-        $state.go(APPSTATE.OVERVIEW, {state: HOMEMODE.MEETINGS});
-    };
+        self.showSignings = function () {
+            $log.debug("menuCtrl: showSignings");
+            $state.go(APPSTATE.OVERVIEW, { state: HOMEMODE.ESIGN });
+        };
 
-    self.showSignings = function() {
-        $log.debug("menuCtrl: showSignings");
-        $state.go(APPSTATE.OVERVIEW, {state: HOMEMODE.ESIGN});
-    };
+        self.showInfo = function () {
+            $log.debug("menuCtrl: showInfo");
+            $state.go(APPSTATE.INFO);
+        };
 
-    self.showInfo = function() {
-        $log.debug("menuCtrl: showInfo");
-        $state.go(APPSTATE.INFO);
-    };
-
-    getMeetings();
-    getOpenSignings();
-}]);
+        getMeetings();
+        getOpenSignings();
+    }]);
