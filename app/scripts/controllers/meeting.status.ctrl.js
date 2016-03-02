a1b2383bc6da9bcaaae0cbecdc2050d080eab318
Hannu Honka <hannu.honka@tieto.com>
2016-03-02 06:37:31 +0200

    meeting.status.ctrl.js and meeting.status.html formatted

--- app/scripts/controllers/meeting.status.ctrl.js
+++ app/scripts/controllers/meeting.status.ctrl.js
@@ -12,98 +12,98 @@
 * Controller of the dashboard
 */
 angular.module('dashboard')
-.controller('meetingStatusCtrl',['$log','$scope','$rootScope','$stateParams','DEVICE','$state','MENU','StorageSrv','ENV', 'APPSTATE','TOPICSTATUS','MTGROLE','KEY','MTGSTATUS','AhjoMeetingSrv', function ($log, $scope, $rootScope, $stateParams, DEVICE, $state, MENU, StorageSrv, ENV, APPSTATE, TOPICSTATUS, MTGROLE, KEY, MTGSTATUS, AhjoMeetingSrv) {
-    $log.debug("meetingStatusCtrl: CONTROLLER");
-    var self = this;
-    $rootScope.menu = $stateParams.menu;
-    self.title = 'MOBILE TITLE';
-    self.meeting = {};
-    self.chairman = false;
-    var meetingItem = $stateParams.meetingItem;
-    var mobile = $rootScope.mobile;
+    .controller('meetingStatusCtrl', ['$log', '$scope', '$rootScope', '$stateParams', 'DEVICE', '$state', 'MENU', 'StorageSrv', 'ENV', 'APPSTATE', 'TOPICSTATUS', 'MTGROLE', 'KEY', 'MTGSTATUS', 'AhjoMeetingSrv', function ($log, $scope, $rootScope, $stateParams, DEVICE, $state, MENU, StorageSrv, ENV, APPSTATE, TOPICSTATUS, MTGROLE, KEY, MTGSTATUS, AhjoMeetingSrv) {
+        $log.debug("meetingStatusCtrl: CONTROLLER");
+        var self = this;
+        $rootScope.menu = $stateParams.menu;
+        self.title = 'MOBILE TITLE';
+        self.meeting = {};
+        self.chairman = false;
+        var meetingItem = $stateParams.meetingItem;
+        var mobile = $rootScope.mobile;
 
-    for (var i = 0; i < ENV.SupportedRoles.length; i++) {
-        if (ENV.SupportedRoles[i].RoleID === MTGROLE.CHAIRMAN) {
-            self.chairman = true;
+        for (var i = 0; i < ENV.SupportedRoles.length; i++) {
+            if (ENV.SupportedRoles[i].RoleID === MTGROLE.CHAIRMAN) {
+                self.chairman = true;
+            }
         }
-    }
 
-    if (meetingItem) {
-        AhjoMeetingSrv.getMeeting(meetingItem.meetingGuid)
-        .then(function(response) {
-            $log.debug("meetingStatusCtrl: getMeeting then:");
-            if (response && response.objects instanceof Array && response.objects.length) {
-                self.meeting = response.objects[0];
-                if (self.meeting && self.meeting.topicList.length) {
-                    var topic = self.meeting.topicList[0];
-                    if (!mobile) {
-                        StorageSrv.set(KEY.TOPIC, topic);
+        if (meetingItem) {
+            AhjoMeetingSrv.getMeeting(meetingItem.meetingGuid)
+                .then(function (response) {
+                    $log.debug("meetingStatusCtrl: getMeeting then:");
+                    if (response && response.objects instanceof Array && response.objects.length) {
+                        self.meeting = response.objects[0];
+                        if (self.meeting && self.meeting.topicList.length) {
+                            var topic = self.meeting.topicList[0];
+                            if (!mobile) {
+                                StorageSrv.set(KEY.TOPIC, topic);
+                            }
+                        }
+                    }
+                    else {
+                        self.meeting = {};
+                        StorageSrv.set(KEY.TOPIC, {});
+                    }
+                },
+                    function (error) {
+                        $log.error("meetingStatusCtrl: getMeeting error: " + JSON.stringify(error));
+                        self.error = error;
+                    },
+                    function (notify) {
+                        $log.debug("meetingStatusCtrl: getMeeting notify: " + JSON.stringify(notify));
+                    })
+                .finally(function () {
+                    $log.debug("meetingStatusCtrl: getMeeting finally: ");
+                });
+        }
+        else {
+            $state.go(APPSTATE.HOME, { menu: MENU.CLOSED });
+        }
+
+        self.goHome = function () {
+            $state.go(APPSTATE.HOME, { menu: MENU.CLOSED });
+        };
+
+        self.topicSelected = function (topic) {
+            StorageSrv.set(KEY.TOPIC, topic);
+            if (mobile) {
+                $state.go(APPSTATE.MEETINGDETAILS, {});
+            }
+        };
+
+        self.isSelected = function (topic) {
+            var selected = StorageSrv.get(KEY.TOPIC);
+            if (topic instanceof Object && selected instanceof Object) {
+                return (topic.topicGuid && topic.topicGuid === selected.topicGuid);
+            }
+            return false;
+        };
+
+        self.statusIcon = function (topic) {
+            for (var item in TOPICSTATUS) {
+                if (TOPICSTATUS.hasOwnProperty(item)) {
+                    if (topic && topic.topicStatus && topic.topicStatus === TOPICSTATUS[item].value) {
+                        return TOPICSTATUS[item].iconPath;
                     }
                 }
             }
-            else {
-                self.meeting = {};
-                StorageSrv.set(KEY.TOPIC, {});
+            return null;
+        };
+
+        self.stringId = function (meeting) {
+            for (var item in MTGSTATUS) {
+                if (MTGSTATUS.hasOwnProperty(item)) {
+                    if (meeting && meeting.meetingStatus && meeting.meetingStatus === MTGSTATUS[item].value) {
+                        return MTGSTATUS[item].stringId;
+                    }
+                }
             }
-        },
-        function(error) {
-            $log.error("meetingStatusCtrl: getMeeting error: " +JSON.stringify(error));
-            self.error = error;
-        },
-        function(notify) {
-            $log.debug("meetingStatusCtrl: getMeeting notify: " +JSON.stringify(notify));
-        })
-        .finally(function() {
-            $log.debug("meetingStatusCtrl: getMeeting finally: ");
+            return '';
+        };
+
+        $scope.$on('$destroy', function () {
+            $log.debug("meetingStatusCtrl: DESTROY");
         });
-    }
-    else {
-        $state.go(APPSTATE.HOME, {menu: MENU.CLOSED});
-    }
 
-    self.goHome = function() {
-        $state.go(APPSTATE.HOME, {menu: MENU.CLOSED});
-    };
-
-    self.topicSelected = function(topic) {
-        StorageSrv.set(KEY.TOPIC, topic);
-        if (mobile) {
-            $state.go(APPSTATE.MEETINGDETAILS, {});
-        }
-    };
-
-    self.isSelected = function(topic) {
-        var selected = StorageSrv.get(KEY.TOPIC);
-        if (topic instanceof Object && selected instanceof Object) {
-            return (topic.topicGuid && topic.topicGuid === selected.topicGuid);
-        }
-        return false;
-    };
-
-    self.statusIcon = function(topic) {
-        for (var item in TOPICSTATUS) {
-            if( TOPICSTATUS.hasOwnProperty(item) ) {
-                if (topic && topic.topicStatus && topic.topicStatus === TOPICSTATUS[item].value) {
-                    return TOPICSTATUS[item].iconPath;
-                }
-            }
-        }
-        return null;
-    };
-
-    self.stringId = function(meeting) {
-        for (var item in MTGSTATUS) {
-            if( MTGSTATUS.hasOwnProperty(item) ) {
-                if (meeting && meeting.meetingStatus && meeting.meetingStatus === MTGSTATUS[item].value) {
-                    return MTGSTATUS[item].stringId;
-                }
-            }
-        }
-        return '';
-    };
-
-    $scope.$on('$destroy', function() {
-        $log.debug("meetingStatusCtrl: DESTROY");
-    });
-
-}]);
+    }]);
