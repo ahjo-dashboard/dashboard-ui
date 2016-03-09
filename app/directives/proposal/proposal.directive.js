4198aff493afd55ebb6c60fe42a1b336324b205c
Hannu Honka <hannu.honka@tieto.com>
2016-03-09 04:51:06 +0200

    proposals implemented

---
+++ app/directives/proposal/proposal.directive.js
@@ -0,0 +1,135 @@
+/**
+ * (c) 2016 Tieto Finland Oy
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name dashboard.proposal:proposalDirective
+ * @description
+ * # proposalDirective
+ */
+angular.module('dashboard')
+    .constant('PROP', {
+        'MODE': {
+            COLLAPSED: 'COLLAPSED',
+            OPEN: 'OPEN',
+            EDITOR: 'EDITOR'
+        }
+    })
+    .filter('unsafe', function($sce) {
+        return function(val) {
+            return $sce.trustAsHtml(val);
+        };
+    })
+    .directive('dbProposal', [function() {
+
+        var controller = ['$log', '$scope', 'PROPS', 'PROP','$rootScope', function($log, $scope, PROPS, PROP, $rootScope) {
+            $log.debug("dbProposal: CONTROLLER");
+            var self = this;
+
+            self.testUserGuid = "926eee0b-8e94-4a14-beec-d9b60590547f"; // Elina Aalto
+
+            self.mode = PROP.MODE.COLLAPSED;
+            self.mds = PROP.MODE;
+            self.isPublished = false;
+            self.isUser = false;
+            self.editedText = "";
+            self.smallEditor = {
+                'menu': [
+                    ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript']
+                ]
+            };
+            self.editor = {
+                'menu': [
+                    // ['bold', 'italic', 'underline', 'strikethrough', 'subscript', 'superscript'],
+                    // ['format-block'],
+                    // ['font'],
+                    // ['font-size'],
+                    // ['font-color', 'hilite-color'],
+                    // ['remove-format'],
+                    // ['ordered-list', 'unordered-list', 'outdent', 'indent'],
+                    // ['left-justify', 'center-justify', 'right-justify']
+                ]
+            };
+
+            function setProposal(proposal) {
+                if (proposal instanceof Object) {
+                    self.isPublished = (proposal.isPublished === PROPS.PUBLISHED.YES);
+                    self.isUser = (proposal.personGuid === self.testUserGuid);
+                    self.editedText = proposal.text;
+
+                    if (proposal.isNew) {
+                        self.mode = PROP.MODE.EDITOR;
+                        delete proposal.isNew;
+                    }
+                }
+            }
+
+            self.startEditing = function() {
+                self.editedText = $scope.proposal.text;
+                self.mode = PROP.MODE.EDITOR;
+            };
+
+            self.endEditing = function() {
+                if ($scope.proposal.text !== self.editedText) {
+                    $scope.proposal.text = self.editedText;
+                    $scope.onPost({ proposal: $scope.proposal });
+                }
+                self.mode = PROP.MODE.OPEN;
+            };
+
+            self.cancelEditing = function() {
+                self.mode = (self.editedText && self.editedText.length) ? PROP.MODE.OPEN : PROP.MODE.COLLAPSED;
+            };
+
+            self.deleteProposal = function() {
+                $scope.onDelete({ proposal: $scope.proposal });
+            };
+
+            self.shareProposal = function() {
+                $scope.proposal.isPublished = PROPS.PUBLISHED.YES;
+                $scope.onPost({ proposal: $scope.proposal });
+            };
+
+            self.open = function() {
+                self.mode = PROP.MODE.OPEN;
+            };
+
+            self.collapse = function() {
+                self.mode = PROP.MODE.COLLAPSED;
+            };
+
+            self.typeText = function(value) {
+                var obj  = $rootScope.objWithVal(PROPS.TYPE, 'value', value);
+                return (obj && obj.text) ? obj.text : value;
+            };
+
+            $scope.$watch(function() {
+                return {
+                    proposal: $scope.proposal
+                };
+            }, function(data) {
+                setProposal(data.proposal);
+            }, true);
+
+            $scope.$on('$destroy', function() {
+                $log.debug("dbProposal: DESTROY");
+            });
+        }];
+
+        return {
+            scope: {
+                proposal: '=',
+                guid: '=',
+                onPost: '&',
+                onDelete: '&'
+            },
+            templateUrl: 'directives/proposal/proposal.Directive.html',
+            restrict: 'AE',
+            controller: controller,
+            controllerAs: 'c',
+            replace: 'true'
+        };
+    }]);
--- app/scripts/services/ahjo.proposals.srv.js
+++ app/scripts/services/ahjo.proposals.srv.js
@@ -12,29 +12,17 @@
 * Service in the dashboard.
 */
 angular.module('dashboard')
-    .service('AhjoProposalsSrv', ['$log', '$http', 'ENV', '$q', '$timeout', function ($log, $http, ENV, $q, $timeout) {
-        $log.log("AhjoProposalsSrv: SERVICE");
-        var self = this;
-
-        self.get = function (guid) {
-            $log.debug("AhjoProposalsSrv: get " + guid);
-            var deferred = $q.defer();
-            $timeout(function () {
-                deferred.notify({});
-                $http({
-                    method: 'GET',
-                    cache: false,
-                    url: ENV.AhjoApi_Proposals.replace('{GUID}', guid)
-                }).then(function (response) {
-                    $log.debug("AhjoProposalsSrv: get then");
-                    deferred.resolve(response.data);
-                }, function (error) {
-                    $log.error("AhjoProposalsSrv: get error");
-                    deferred.reject(error);
-                });
-            }, 0);
-
-            return deferred.promise;
-        };
-
+    .factory('AhjoProposalsSrv', ['$resource', 'ENV', function ($resource, ENV) {
+        return $resource(ENV.AhjoApi_Proposals, { guid: '@guid' }, {
+            get: {
+                method: ENV.HTTP_GET,
+                cache: false
+            },
+            post: {
+                method: ENV.HTTP_POST
+            },
+            delete: {
+                method: ENV.HTTP_DELETE
+            }
+        });
     }]);
