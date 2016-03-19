f7821e9ef952a0f352045e53215a616f1b8f024b
Hannu Honka <hannu.honka@tieto.com>
2016-03-19 05:33:08 +0200

    user selection stored in overview

--- app/scripts/controllers/overview.ctrl.js
+++ app/scripts/controllers/overview.ctrl.js
@@ -12,16 +12,30 @@
  * Controller of the dashboard
  */
 angular.module('dashboard')
-    .controller('overviewCtrl', ['$scope', '$log', 'ENV', 'SigningOpenApi', '$state', '$rootScope', 'CONST', '$stateParams', 'MTGD', function($scope, $log, ENV, SigningOpenApi, $state, $rootScope, CONST, $stateParams, MTGD) {
+    .controller('overviewCtrl', ['$scope', '$log', 'ENV', 'SigningOpenApi', '$state', '$rootScope', 'CONST', '$stateParams', 'MTGD', 'StorageSrv', function($scope, $log, ENV, SigningOpenApi, $state, $rootScope, CONST, $stateParams, MTGD, StorageSrv) {
         $log.debug("overviewCtrl: CONTROLLER, mode: ", $stateParams.state);
         var self = this;
         self.loading = 0;
         self.signReqsHeader = 'Avoimet allekirjoituspyynnöt';
         self.signErr = null;
         self.blockMode = CONST.BLOCKMODE.BOTH;
-        self.vblMtgs = MTGD.VISIBLE.OPEN;
         self.vbl = MTGD.VISIBLE;
-        self.closedSignReqs = false;
+
+        var visibleMtgs = StorageSrv.get(CONST.KEY.VISIBLE_MTGS);
+        switch (visibleMtgs) {
+            case MTGD.VISIBLE.OPEN:
+                self.vblMtgs = MTGD.VISIBLE.OPEN;
+                break;
+            case MTGD.VISIBLE.CLOSED:
+                self.vblMtgs = MTGD.VISIBLE.CLOSED;
+                break;
+            default:
+                self.vblMtgs = MTGD.VISIBLE.OPEN;
+                break;
+        }
+
+        var signingRes = StorageSrv.get(CONST.KEY.SIGNING_RES);
+        self.closedSignReqs = signingRes ? signingRes : false;
 
         $rootScope.menu = CONST.MENU.CLOSED;
         var mode = $stateParams.state;
@@ -70,6 +84,12 @@ angular.module('dashboard')
 
         self.setMtgsVisible = function(mtgs) {
             self.vblMtgs = mtgs;
+            StorageSrv.set(CONST.KEY.VISIBLE_MTGS, mtgs);
+        };
+
+        self.setClosedSignReqs = function(value) {
+            self.closedSignReqs = value;
+            StorageSrv.set(CONST.KEY.SIGNING_RES, value);
         };
 
         $scope.$on('$destroy', function() {
