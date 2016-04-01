c27f6d1273b713855247674f53c4d5575b4e21a4
Hannu Honka <hannu.honka@tieto.com>
2016-04-01 12:27:26 +0300

    list view changed to use db-link direcitve

--- app/scripts/controllers/list.ctrl.js
+++ app/scripts/controllers/list.ctrl.js
@@ -8,20 +8,12 @@
  * Controller of the dashboard
  */
 angular.module('dashboard')
-    .controller('listCtrl', ['$log', '$scope', 'StorageSrv', 'CONST', '$state', function ($log, $scope, StorageSrv, CONST, $state) {
+    .controller('listCtrl', ['$log', '$scope', 'StorageSrv', 'CONST', function($log, $scope, StorageSrv, CONST) {
         $log.debug("listCtrl: CONTROLLER");
         var self = this;
         self.data = StorageSrv.get(CONST.KEY.SELECTION_DATA);
-        self.publicity = CONST.PUBLICITY;
 
-        self.select = function (item) {
-            if (item && item.publicity !== CONST.PUBLICITY.SECRET) {
-                StorageSrv.set(CONST.KEY.LISTPDF_DATA, item);
-                $state.go(CONST.APPSTATE.LISTATTACHMENT);
-            }
-        };
-
-        $scope.$on('$destroy', function () {
+        $scope.$on('$destroy', function() {
             $log.debug("listCtrl: DESTROY");
         });
     }]);
