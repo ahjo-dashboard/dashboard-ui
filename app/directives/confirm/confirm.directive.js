35cb5a8117123bbba2490a2ec5d4861c379ea875
Hannu Honka <hannu.honka@tieto.com>
2016-04-05 12:14:58 +0300

    confirm directive implemented

---
+++ app/directives/confirm/confirm.directive.js
@@ -0,0 +1,85 @@
+/**
+ * (c) 2016 Tieto Finland Oy
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name dashboard.proposals:confirmDirective
+ * @description
+ * # confirmDirective
+ */
+angular.module('dashboard')
+    .directive('dbConfirm', ['$log', function() {
+
+        var controller = ['$log', '$scope', '$uibModal', function($log, $scope, $uibModal) {
+            $log.debug("dbConfirm: CONTROLLER");
+
+            $scope.open = function() {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'directives/confirm/confirm.html',
+                    controller: function($scope, $uibModalInstance, config) {
+                        $scope.title = 'STR_CONFIRM';
+                        $scope.text = 'STR_CNFM_TEXT';
+                        $scope.yes = 'STR_YES';
+                        $scope.no = 'STR_NO';
+
+                        if (config instanceof Object) {
+                            $scope.title = config.title === undefined ? $scope.title : config.title;
+                            $scope.title = config.text === undefined ? $scope.text : config.text;
+                            $scope.yes = config.yes === undefined ? $scope.yes : config.yes;
+                            $scope.no = config.no === undefined ? $scope.no : config.no;
+                        }
+                        $scope.clicked = function(ok) {
+                            if (ok) {
+                                $uibModalInstance.close();
+                            }
+                            else {
+                                $uibModalInstance.dismiss();
+                            }
+                        };
+                    },
+                    resolve: {
+                        config: function() {
+                            return $scope.confirmConfig;
+                        }
+                    }
+                });
+
+                modalInstance.result.then(function() {
+                    $scope.ngClick();
+                });
+
+            };
+
+            $scope.$on('$destroy', function() {
+                $log.debug("dbConfirm: DESTROY");
+            });
+        }];
+
+        return {
+            scope: {
+                confirmActive: '=',
+                confirmConfig: '=',
+                ngClick: '&'
+            },
+            restrict: 'A',
+            controller: controller,
+            replace: 'true',
+            link: function(scope, element/*, attrs*/) {
+                var CLK = "click";
+                element.unbind(CLK).bind(CLK, function($event) {
+                    $event.preventDefault();
+
+                    if (scope.confirmActive === undefined || scope.confirmActive) {
+                        scope.open();
+                    }
+                    else {
+                        scope.$apply(scope.ngClick);
+                    }
+                });
+            }
+        };
+    }]);
