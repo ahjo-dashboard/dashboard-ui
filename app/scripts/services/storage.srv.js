01ebdae14d30b1517d7fc0d8a18fbf7d38985cae
Hannu Honka <hannu.honka@tieto.com>
2016-04-22 09:48:48 +0300

    StorageSrv reset implemented

--- app/scripts/services/storage.srv.js
+++ app/scripts/services/storage.srv.js
@@ -56,6 +56,9 @@ angular.module('dashboard')
                 else {
                     $log.error("StorageSrv: deleteKey invalid parameter");
                 }
+            },
+            reset: function () {
+                data = {};
             }
         };
     });
