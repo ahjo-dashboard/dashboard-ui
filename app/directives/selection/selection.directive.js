0d1bf6f78161a0380fed740d38d3737e70775086
Tommi Kenakkkala <tommi.kenakkala@tieto.com>
2016-04-27 12:00:10 +0300

    Login: login dropdown populated by editing DOM, WP perf workaround
    Works around the bad Windows Phone IE performance of angular ng-repeat
    populating a dropdown with a very big count of options.
    Otherwise on target env page is blocked long.

---
+++ app/directives/selection/selection.directive.js
@@ -0,0 +1,62 @@
+/**
+ * (c) 2016 Tieto Finland Oy
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name dbSelection
+ * @description
+ * # dbSelection
+ */
+angular.module('dashboard')
+    .directive('dbSelection', ['$compile', '$log', '$timeout', function ($compile, $log, $timeout) {
+        return {
+            scope: {
+                selItems: '='
+            },
+            restrict: 'AE',
+            replace: 'true',
+            link: function (scope, el /*, attrs*/) {
+                // $log.debug("dbSelection.link");
+
+                function setChildren(arr) {
+                    if (!angular.isArray(arr)) {
+                        $log.log("dbSelection.setChildren: bad arg");
+                        return;
+                    }
+                    $log.debug("dbSelection.setChildren: options count=" + arr.length);
+
+                    var tmp = '';
+                    for (var i = 0; i < arr.length; i++) {
+                        tmp = tmp + '<option value="' + arr[i].value + '">' + arr[i].text + '</option>';
+                    }
+
+                    el.empty();
+                    el.append($compile(tmp)(scope));
+                }
+
+                if (scope.selItems && angular.isArray(scope.selItems)) {
+                    setChildren(scope.selItems);
+                }
+
+                scope.$watch(
+                    function () {
+                        // Only length watched instead of entire array to avoid digest loops and bad perf.
+                        return scope.selItems.length;
+                    },
+                    function (newVal, oldVal) {
+                        $log.debug("dbSelection items watch triggered " + newVal + " " + oldVal);
+                        if (!angular.equals(newVal, oldVal)) {
+
+                            $timeout(function () {
+                                setChildren(scope.selItems);
+                            }, 0);
+                        }
+                    }, true
+                );
+
+            }
+        };
+    }]);
