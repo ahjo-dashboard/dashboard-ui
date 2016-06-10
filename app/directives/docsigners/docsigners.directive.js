b9a130c67352b2dc9154edc0614000a58cf5c13c
Tommi Kenakkkala <tommi.kenakkala@tieto.com>
2016-06-10 16:30:04 +0300

    Esign: docsigners.directive to get its data from service

--- app/directives/docsigners/docsigners.directive.js
+++ app/directives/docsigners/docsigners.directive.js
@@ -10,56 +10,88 @@
  * @description
  * # docSigners
  */
-angular.module('dashboard')
-    .directive('docSigners', [function () {
+var app = angular.module('dashboard');
+app.directive('docSigners', [function () {
 
-        var controller = ['$log', '$scope', '$rootScope', 'CONST', 'Utils', function ($log, $scope, $rootScope, CONST, Utils) {
-            $log.log("docSigners.CONTROLLER");
+    var ctrl = ['$log', '$scope', '$rootScope', 'CONST', 'Utils', 'SigningDocSignaturesApi', function ($log, $scope, $rootScope, CONST, Utils, SigningDocSignaturesApi) {
+        $log.log("docSigners.CONTROLLER");
 
-            var self = this;
-            self.listModel = $scope.listmodel;
-            self.isMobile = $scope.ismobile;
+        var self = this;
 
-            // $log.debug("docSigners: " + JSON.stringify(self.listModel));
+        if (!$scope.item || !("ProcessGuid" in $scope.item) || !$scope.item.ProcessGuid) {
+            $log.error("signDetailsCtrl.getReqStatuses: bad args");
+            return;
+        }
 
-            /* PRIVATE FUNCTIONS */
+        self.item = $scope.item;
+        self.isMobile = $scope.ismobile;
+        self.signers = null;
+        self.busy = false;
 
 
-            /* PUBLIC FUNCTIONS */
+        // PRIVATE FUNCTIONS
 
-            /* Resolve display text for item status */
-            self.statusStrId = function (value) {
-                var s = Utils.objWithVal(CONST.ESIGNSTATUS, 'value', value);
-                return s ? s.stringId : '';
-            };
+        function getReqStatuses(item, resultCont) {
+            $log.debug("signDetailsCtrl.getReqStatuses");
 
-            self.roleStrId = function (value) {
-                var s = Utils.objWithVal(CONST.ESIGNROLE, 'value', value);
-                return s ? s.stringId : '';
-            };
-
-            self.initiatorRoleStrId = function (docType) {
-                // Special case for request status list roles, initiator role doesn't have a role code
-                return docType && docType === CONST.ESIGNTYPE.OFFICIAL.value ? 'STR_SROLE_PREP' : CONST.ESIGNROLE.SEC.stringId;
-            };
-
-            /* Resolve css class for signing status */
-            self.statusStyle = function (status) {
-                var s = Utils.objWithVal(CONST.ESIGNSTATUS, 'value', status);
-                return s ? s.badgeClass : 'label-default';
-            };
-
-        }];
-
-        return {
-            controller: controller,
-            controllerAs: 'c',
-            templateUrl: 'directives/docsigners/docsigners.directive.html',
-            restrict: 'E',
-            replace: 'true',
-            scope: {
-                listmodel: '=',
-                ismobile: '='
+            if (!item || !("ProcessGuid" in item) || !item.ProcessGuid) {
+                $log.error("signDetailsCtrl.getReqStatuses: bad args");
+                return;
             }
+
+            resultCont.busy = true;
+            var prom = SigningDocSignaturesApi.get({ reqId: item.ProcessGuid }, function (data) {
+                $log.debug("signDetailsCtrl.getReqStatuses: api query done ");
+                self.signers = data && data.Signers ? data.Signers : null;
+            }, function (error) {
+                $log.error("signDetailsCtrl.getReqStatuses: api query error: " + JSON.stringify(error));
+                //TODO: display error?
+            });
+            prom.$promise.finally(function () {
+                resultCont.busy = false;
+            });
+        }
+
+
+        // PUBLIC FUNCTIONS
+
+        /* Resolve display text for item status */
+        self.statusStrId = function (value) {
+            var s = Utils.objWithVal(CONST.ESIGNSTATUS, 'value', value);
+            return s ? s.stringId : '';
         };
-    }]);
+
+        self.roleStrId = function (value) {
+            var s = Utils.objWithVal(CONST.ESIGNROLE, 'value', value);
+            return s ? s.stringId : '';
+        };
+
+        self.initiatorRoleStrId = function (docType) {
+            // Special case for request status list roles, initiator role doesn't have a role code
+            return docType && docType === CONST.ESIGNTYPE.OFFICIAL.value ? 'STR_SROLE_PREP' : CONST.ESIGNROLE.SEC.stringId;
+        };
+
+        /* Resolve css class for signing status */
+        self.statusStyle = function (status) {
+            var s = Utils.objWithVal(CONST.ESIGNSTATUS, 'value', status);
+            return s ? s.badgeClass : 'label-default';
+        };
+
+        // CONSTRUCTION
+
+        getReqStatuses(self.item, self);
+
+    }];
+
+    return {
+        controller: ctrl,
+        controllerAs: 'c',
+        templateUrl: 'directives/docsigners/docsigners.directive.html',
+        restrict: 'E',
+        replace: 'true',
+        scope: {
+            item: '=',
+            ismobile: '='
+        }
+    };
+}]);
